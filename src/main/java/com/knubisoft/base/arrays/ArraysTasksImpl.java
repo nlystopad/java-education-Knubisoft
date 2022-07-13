d5f22aa4b2179698df5dbe2d6d06d0d440ef6e71
nlystopad <nikita12312123@gmail.com>
2022-07-13 15:15:39 +0300

    Initial commit

---
+++ src/main/java/com/knubisoft/base/arrays/ArraysTasksImpl.java
@@ -0,0 +1,72 @@
+package com.knubisoft.base.arrays;
+
+public class ArraysTasksImpl implements ArraysTasks {
+
+    @Override
+    public int[] reverse(int[] array) {
+        if (array.length == 0) {
+            return array;
+        } else {
+            int[] returnArr = new int[array.length];
+            for (int i = 1; i <= array.length; i++) {
+                returnArr[i - 1] = array[array.length - i];
+            }
+            return returnArr;
+        }
+    }
+
+    @Override
+    public int[] mergeArrays(int[] array1, int[] array2) {
+        int[] returnArr = new int[array1.length + array2.length];
+        System.arraycopy(array1, 0, returnArr, 0, array1.length);
+        System.arraycopy(array2, 0, returnArr, array1.length, array2.length);
+        return returnArr;
+    }
+
+    @Override
+    public int[] findMax3InArray(int[] array) {
+        if (array.length < 3) {
+            return array;
+        } else {
+            int[] returnArr = new int[3];
+
+        }
+        return null;
+    }
+
+    @Override
+    public int findLongestIncreasingContinuesSubsequence(int[] array) {
+        return -1;
+    }
+
+    @Override
+    public int sumOfAllUniqueElements(int[] array) {
+        return -1;
+    }
+
+    @Override
+    public int[] moveZeroes(int[] array) {
+        return null;
+    }
+
+    @Override
+    public int findFinalValue(int[] nums, int original) {
+
+        return -1;
+    }
+
+    @Override
+    public String longestCommonPrefix(String[] words) {
+        return null;
+    }
+
+    @Override
+    public int missingNumber(int[] array) {
+        return -1;
+    }
+
+    @Override
+    public boolean containsDuplicate(int[] array) {
+        return false;
+    }
+}
