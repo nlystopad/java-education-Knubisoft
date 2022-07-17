9d9b1b0558c08433a0ddf30d5e94245b08cc9431
nlystopad <nikita12312123@gmail.com>
2022-07-17 19:27:06 +0300

    finished BoolTasksImpl

--- src/main/java/com/knubisoft/base/bool/BoolTasksImpl.java
+++ src/main/java/com/knubisoft/base/bool/BoolTasksImpl.java
@@ -4,34 +4,47 @@ public class BoolTasksImpl implements BoolTasks {
 
     @Override
     public Boolean isTrueAutobox(boolean value) {
-        return null;
+        return (Boolean) value;
     }
 
     @Override
     public Boolean isFalseAutobox(boolean value) {
-        return null;
+        return (Boolean) value;
     }
 
     @Override
     public boolean isTrueUnbox(Boolean value) {
-        return false;
+        return value;
     }
 
     @Override
     public Boolean isFalseUnbox(boolean value) {
-        return null;
+        return value;
     }
 
     @Override
     public boolean andFunction(int digit, String string) {
-        return false;
+        if (string == null) {
+            return false;
+        }
+        return String.valueOf(digit).length() == string.length() && Integer.parseInt(string) == digit;
     }
 
     @Override
     public boolean orFunction(int digit, String string) {
-        return false;
+        if (string == null || String.valueOf(digit).length() != string.length()) {
+            return false;
+        }
+        boolean returnVal;
+        try {
+            returnVal = Integer.parseInt(string) == digit;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return returnVal;
     }
 
+    // kinda weird, don't understand task (for next1 also)
     @Override
     public boolean andComplexFunction(int generatedFirstDigit, double generatedSecondDigit, int range) {
         return false;
@@ -44,11 +57,17 @@ public class BoolTasksImpl implements BoolTasks {
 
     @Override
     public boolean isSameSizeArray(Object[] firstArray, Object... secondArray) {
-        return false;
+        if (firstArray == null || secondArray == null) {
+            return false;
+        }
+        return firstArray.length == secondArray.length;
     }
 
     @Override
     public boolean isSameCharactersCount(String username, String name, int maxLength) {
-        return false;
+        if (maxLength == 0 || username == null || name == null || username.length() == 0 || name.length() == 0) { // todo : too much conditions in 1 if, should be refactored
+            return false;
+        }
+        return username.length() == name.length();
     }
 }
