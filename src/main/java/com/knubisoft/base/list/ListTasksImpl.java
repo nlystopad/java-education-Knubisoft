35ec874d4b6c9cfb0c98d6b03e1afaa5dab0ea18
nlystopad <nikita12312123@gmail.com>
2022-08-08 21:20:36 +0300

    added list tasks

--- src/main/java/com/knubisoft/base/list/ListTasksImpl.java
+++ src/main/java/com/knubisoft/base/list/ListTasksImpl.java
@@ -1,9 +1,6 @@
 package com.knubisoft.base.list;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ListTasksImpl implements ListTasks {
     @Override
@@ -94,57 +91,117 @@ public class ListTasksImpl implements ListTasks {
             if (s == null) {
                 throw new NullPointerException();
             }
-           try {
-               returnList.add(Long.parseLong(s));
-           } catch (NumberFormatException e){
-               throw new IllegalArgumentException();
-           }
+            try {
+                returnList.add(Long.parseLong(s));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException();
+            }
         }
         return returnList;
     }
 
     @Override
     public int findMaxValue(List<Integer> first, List<Integer> second, List<Integer> third) {
-        return -1;
+        if (first == null || second == null || third == null) {
+            throw new RuntimeException();
+        }
+        first.addAll(second);
+        first.addAll(third);
+        return Collections.max(first);
     }
 
     @Override
     public int findMinValue(List<Integer> first, List<Integer> second, List<Integer> third) {
-        return -1;
+        if (first == null || second == null || third == null) {
+            throw new RuntimeException();
+        }
+        first.addAll(second);
+        first.addAll(third);
+        return Collections.min(first);
     }
 
     @Override
     public int multiplyMax2Elements(List<Integer> first, List<Integer> second, List<Integer> third) {
-        return -1;
+        if (first == null || second == null || third == null) {
+            throw new RuntimeException();
+        }
+        first.addAll(second);
+        first.addAll(third);
+        Collections.sort(first);
+        Collections.reverse(first);
+        return first.get(0) * first.get(1);
     }
 
     @Override
     public List<String> removeNulls(List<String> list) {
-        return null;
+        if (list == null) {
+            throw new RuntimeException();
+        }
+        list.removeIf(Objects::isNull);
+        return list;
     }
 
     @Override
     public List<Integer> flatMapWithoutNulls(List<List<Integer>> list) {
-        return null;
+        if (list == null) {
+            throw new NoSuchElementException();
+        }
+        List<Integer> integers = new ArrayList<>();
+        list.stream().forEach(entry -> entry.stream().forEach(val -> {
+            if (val == null) {
+                integers.add(val);
+            }
+        }));
+        return integers;
     }
 
     @Override
     public List<Integer> cloneIds(List<Integer> originalIds) {
-        return null;
+        if (originalIds == null) {
+            throw new NoSuchElementException();
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer i : originalIds) {
+            if (i != null) {
+                list.add(i);
+            }
+        }
+        return list;
     }
 
     @Override
     public List<String> shuffle(List<String> originalStrings) {
-        return null;
+        if (originalStrings == null) {
+            throw new RuntimeException();
+        }
+        Collections.shuffle(originalStrings);
+        return originalStrings;
     }
 
     @Override
     public String getLastElement(LinkedList<String> list) {
-        return null;
+        if (list == null) {
+            throw new NoSuchElementException();
+        }
+        if (list.size() == 0 ) {
+            return "";
+        }
+        return list.get(list.size() - 1);
     }
 
     @Override
     public List<String> compareElements(LinkedList<String> originalCollection, LinkedList<String> additionalCollection) {
-        return null;
+        if (originalCollection == null || additionalCollection == null) {
+            throw new IllegalArgumentException();
+        }
+        List<String> strings = new LinkedList<>(); // n^2, todo: rewrite to make it faster
+        for (String s : originalCollection) {
+            for (String s1: additionalCollection) {
+                if (Objects.equals(s, s1)) {
+                   strings.add(s);
+                }
+            }
+        }
+        return strings;
     }
 }
