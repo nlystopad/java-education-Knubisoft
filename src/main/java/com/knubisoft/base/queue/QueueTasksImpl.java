e66f2d99138a20ec29fa5c517fd29be93d018031
nlystopad <nikita12312123@gmail.com>
2022-11-02 22:37:03 +0200

    added queue tasks

--- src/main/java/com/knubisoft/base/queue/QueueTasksImpl.java
+++ src/main/java/com/knubisoft/base/queue/QueueTasksImpl.java
@@ -1,36 +1,81 @@
 package com.knubisoft.base.queue;
 
 import com.knubisoft.base.queue.car.Car;
+import com.knubisoft.base.queue.car.CarComparator;
 
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class QueueTasksImpl implements QueueTasks {
 
     @Override
     public Queue<Integer> reverseQueueUsingRecursion(Queue<Integer> queue) {
-        return null;
+        if (queue.isEmpty()) {
+            return queue;
+        }
+        Integer data = queue.peek();
+        queue.remove();
+        queue = reverseQueueUsingRecursion(queue);
+        queue.add(data);
+        return queue;
     }
 
     @Override
     public Queue<Integer> reverseFirstKElementsOfQueue(Queue<Integer> queue, int k) {
-        return null;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            stack.push(queue.peek());
+            queue.remove();
+        }
+        while (!stack.isEmpty()) {
+            queue.add(stack.peek());
+            stack.pop();
+        }
+        for (int i = 0; i < queue.size() - k; i++) {
+            queue.add(queue.peek());
+            queue.remove();
+        }
+        return queue;
     }
 
     @Override
     public Queue<Integer> sortQueueOfInt(Queue<Integer> queue) {
-        return null;
+        return new LinkedList<>(new TreeSet<>(queue));
     }
 
     @Override
     public boolean validParentheses(String parentheses) {
-        return false;
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < parentheses.length(); i++) {
+            char x = parentheses.charAt(i);
+            if (x == '(' || x == '[' || x == '{') {
+                deque.push(x);
+            }
+            if (deque.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = deque.pop();
+                    if (check == '{' || check == '[') return false;
+                    break;
+                case '}':
+                    check = deque.pop();
+                    if (check == '(' || check == '[') return false;
+                    break;
+                case ']':
+                    check = deque.pop();
+                    if (check == '(' || check == '{') return false;
+                    break;
+            }
+        }
+        return deque.isEmpty();
     }
 
     @Override
     public PriorityQueue<Car> implementPriorityQueueThroughComparator(List<Car> cars) {
-        return null;
+        PriorityQueue<Car> priorityQueue = new PriorityQueue<>(new CarComparator());
+        priorityQueue.addAll(cars);
+        return priorityQueue;
     }
 
 }
